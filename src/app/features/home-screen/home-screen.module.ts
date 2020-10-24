0eb663c9c2de81f6a5bd704699fb742ff1dd8df3
Ghassen BARGOUGUI <ghassen.brg16@gmail.com>
2020-10-24 20:39:43 +0100

    Tabs Translation

--- src/app/features/home-screen/home-screen.module.ts
+++ src/app/features/home-screen/home-screen.module.ts
@@ -7,9 +7,19 @@ import { IonicModule } from '@ionic/angular';
 import { HomeScreenPageRoutingModule } from './home-screen-routing.module';
 
 import { HomeScreenPage } from './home-screen.page';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'src/app/core/services/i18n/httpLoaderFactory';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   imports: [
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     CommonModule,
     FormsModule,
     IonicModule,
